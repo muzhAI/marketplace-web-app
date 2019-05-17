f2fd21884cd8f9c429392c2deb64262500c7e3bf
Muzh Andriy <muzhandriy@gmail.com>
2019-05-17 14:32:38 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Web Marketplace App
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/main.scss';
+
+function App() {
+  return (
+    <h1>Web Marketpace App</h1>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
